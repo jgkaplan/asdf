c5a61f64cdc4eb651afa4e65f8601c7dacd4bac5
jgkaplan <joshuagraykaplan@gmail.com>
2016-06-16 20:34:22 -0700

    Added two more colors and option to switch between word and letter coloring

--- index.js
+++ index.js
@@ -3,21 +3,30 @@
 
 const chalk = require('chalk');
 const keypress = require('keypress');
-
+const program  = require('commander');
+const colors = [chalk.red, chalk.green, chalk.yellow, chalk.blue, chalk.magenta, chalk.cyan, chalk.gray, chalk.white, chalk.black];
+let type = process.argv.indexOf('-w')!=-1?'w':'l';
 keypress(process.stdin);
-
-let colors = [chalk.red, chalk.green, chalk.yellow, chalk.blue, chalk.magenta, chalk.cyan, chalk.gray];
-let currentColor = colors[Math.floor(Math.random()*colors.length)]
-
+function randomColor(){
+	return colors[Math.floor(Math.random()*colors.length)]
+}
+let currentColor = randomColor();
+program.option('-w, --word', 'Change color every word (default is every letter)').action(function() {}).parse(process.argv);
 process.stdin.on('keypress', function(ch,key) {
 	if (key){
 		if ((key.ctrl && key.name=='c') || key.name=='escape'){
 			process.stdin.pause();
 		} else if (key.name=="return"){
 			process.stdout.write('\n');
+		} else if (key.name=='space'){
+			currentColor = randomColor();
 		}
 	}
-	process.stdout.write(colors[Math.floor(Math.random()*colors.length)](ch));
+	if (type == 'w'){
+		process.stdout.write(currentColor(ch));
+	}else{
+		process.stdout.write((randomColor())(ch));
+	}
 });
 
 process.stdin.setRawMode(true);
